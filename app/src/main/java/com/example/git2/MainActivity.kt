3ed38e4a06da6d1d9a228f4b0f0f16fcbcb0fffa
Helmy7G <helmyboshnaq2222@gmail.com>
2022-02-20 12:23:06 +0200

    new pro

--- app/src/main/java/com/example/git2/MainActivity.kt
+++ app/src/main/java/com/example/git2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     var test = "asfagasg"
+    var test1 = 32135
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
